a38b69524abbf04b16bd2d998e79a6b0925c6a5d
bkrajor <bkrajor@gmail.com>
2022-05-05 02:46:55 +0530

    WorkInProgress

--- src/models/collegeModel.js
+++ src/models/collegeModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const collegeModel = mongoose.Schema({
     name: {
-        type: String, unique: true, trim: true    
+        type: String, required:true, unique: true, trim: true   
     },
     fullName: { 
         type: String, required: true, trim: true 
@@ -11,8 +11,7 @@ const collegeModel = mongoose.Schema({
         type: String, required: true 
     },
     isDeleted: { 
-        type: Boolean
-        , default: false 
+        type: Boolean, default: false 
     }
 
 }, { timestamps: true });
